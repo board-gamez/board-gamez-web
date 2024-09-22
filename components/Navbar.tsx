b9ca63ac2f7d7f637e950d59546ef74d5916ea0a
Mahdi Razaqi <mahdirazaqi77@gmail.com>
2024-09-22 18:19:45 +0330

    first code

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="bg-red-300 container mx-auto">
+      <nav className="flex">
+        <div className="bg-blue-200">1</div>
+        <div className="bg-yellow-200">2</div>
+        <div className="bg-purple-200">3</div>
+      </nav>
+    </div>
+  );
+}
