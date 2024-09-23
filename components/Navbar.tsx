23ba96f1ba1db9a0df90ac5e75fc49653e072997
Mahdi Razaqi <mahdirazaqi77@gmail.com>
2024-09-23 14:06:43 +0330

    add vazirmatn font

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,41 @@ import Image from "next/image";
 import SearchIcon from "./icons/Search";
 import CartIcon from "./icons/Cart";
 import UserIcon from "./icons/User";
+import Link from "next/link";
 
 export default function Navbar() {
+  const navItems = [
+    {
+      id: 1,
+      name: "تماس با ما",
+      link: "/contact-us",
+    },
+    {
+      id: 2,
+      name: "درباره با ما",
+      link: "/about-us",
+    },
+  ];
+
   return (
-    <nav className="bg-red-300 container mx-auto flex">
-      <div className="bg-blue-200 flex-none">
-        <Image src={"/logo.svg"} width={100} height={100} alt="hello" />
+    <nav className="bg-red-300 container mx-auto flex items-center">
+      <Link href={"/"} className="bg-blue-200 p-2">
+        <Image src={"/logo.svg"} width={80} height={80} alt="" />
+      </Link>
+
+      <div className="bg-yellow-200 flex-1">
+        {navItems.map((navItem) => (
+          <Link
+            key={navItem.id}
+            href={navItem.link}
+            className="inline-block px-3 p-2"
+          >
+            {navItem.name}
+          </Link>
+        ))}
       </div>
-      <div className="bg-yellow-200 flex-1">2</div>
-      <div className="bg-purple-200 flex-none flex items-center">
+
+      <div className="bg-purple-200 flex">
         <SearchIcon />
         <CartIcon />
         <UserIcon />
