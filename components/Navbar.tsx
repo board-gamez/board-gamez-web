c5c45e1c6299e9629ba0b115eb754a876ea2caff
Mahdi Razaqi <mahdirazaqi77@gmail.com>
2024-09-23 09:50:38 +0330

    work in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
+import SearchIcon from "./icons/Search";
+import CartIcon from "./icons/Cart";
+import UserIcon from "./icons/User";
+
 export default function Navbar() {
   return (
-    <div className="bg-red-300 container mx-auto">
-      <nav className="flex">
-        <div className="bg-blue-200">1</div>
-        <div className="bg-yellow-200">2</div>
-        <div className="bg-purple-200">3</div>
-      </nav>
-    </div>
+    <nav className="bg-red-300 container mx-auto flex">
+      <div className="bg-blue-200 flex-none">
+        <Image src={"/logo.svg"} width={100} height={100} alt="hello" />
+      </div>
+      <div className="bg-yellow-200 flex-1">2</div>
+      <div className="bg-purple-200 flex-none flex items-center">
+        <SearchIcon />
+        <CartIcon />
+        <UserIcon />
+      </div>
+    </nav>
   );
 }
