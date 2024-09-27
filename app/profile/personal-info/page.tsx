698e68e1e82ffa6beedc2105eb84941089e11129
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2024-09-27 16:42:40 +0330

    ADD: personal info page

--- app/profile/personal-info/page.tsx
+++ app/profile/personal-info/page.tsx
@@ -1,44 +1,29 @@
-import NumberInput from "@/components/NumberInput";
-import { products } from "@/fake-data/product.data";
-import Image from "next/image";
-
 export default function Profile() {
-  const productsCost = 270000;
-  const shippingCost = 45000;
-  const totalCost = productsCost + shippingCost;
-
   return (
-    <>
-      <div className="flex text-center font-bold border-b p-2">
-        <div className="flex-none w-28 p-3">تصویر</div>
-        <div className="flex-1 p-3">نام محصول</div>
-        <div className="flex-none w-28 p-3">تعداد</div>
-        <div className="flex-none w-36 p-3">قیمت</div>
+    <form className="grid grid-cols-2 gap-3 p-4">
+      <div className="flex flex-col">
+        <label className="font-bold mb-2">نام و نام خانوادگی</label>
+        <input
+          type="text"
+          placeholder="نام و نام خانوادگی"
+          className="bg-light-gray border p-3 rounded-lg"
+        />
       </div>
 
-      {products.map((product) => (
-        <div
-          key={product.slug}
-          className="flex items-center text-center font-bold border-b p-2"
-        >
-          <div className="flex-none w-28">
-            <Image
-              src={product.image}
-              alt={product.title}
-              width={150}
-              height={150}
-              className="w-full aspect-square"
-            />
-          </div>
-          <div className="flex-1 p-3">{product.title}</div>
-          <div className="flex-none w-28 p-3">
-            <NumberInput max={product.quantity} />
-          </div>
-          <div className="flex-none w-36 p-3">
-            {product.price.toLocaleString()} تومان
-          </div>
-        </div>
-      ))}
-    </>
+      <div className="flex flex-col">
+        <label className="font-bold mb-2">شماره موبایل</label>
+        <input
+          type="text"
+          placeholder="شماره موبایل"
+          value={"09196062361"}
+          className="bg-light-gray border p-3 rounded-lg"
+          disabled
+        />
+      </div>
+
+      <button className="bg-black w-40 text-white rounded-lg p-3 mt-2">
+        ویرایش اطلاعات
+      </button>
+    </form>
   );
 }
