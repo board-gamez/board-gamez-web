ca4902447b9545b3417e42c1ffa0b1685723f1c0
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2024-10-19 19:33:07 +0330

    finalize auth login page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,27 +2,28 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 type Inputs = {
   phone: string;
+  code: string;
 };
 
 export default function AuthPage() {
   const [step, setStep] = useState(1);
+  const [region, setRegion] = useState("IR");
+  const [phone, setPhone] = useState("");
+
   const { register, handleSubmit } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
-  };
-
-  const onSubmitSendCode = () => {
-    console.log("----");
-
+  const onSubmitSendCode = ({ phone }: { phone: string }) => {
+    setPhone(phone);
     setStep(2);
   };
 
-  const onSubmitVerifyCode = () => {};
+  const onSubmitVerifyCode = ({ code }: { code: string }) => {
+    console.log(region, phone, code);
+  };
 
   return (
     <main>
@@ -45,6 +46,7 @@ export default function AuthPage() {
             type="text"
             className="bg-light-gray rounded-lg p-4 outline-none mb-7"
             placeholder="شماره موبایل خود را وارد کنید"
+            maxLength={11}
             {...register("phone", { required: true })}
           />
 
@@ -59,7 +61,7 @@ export default function AuthPage() {
 
       {step === 2 && (
         <form
-          onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit(onSubmitVerifyCode)}
           className="bg-white w-[500px] mx-auto rounded-lg px-5 flex flex-col py-16 my-20"
         >
           <Image
@@ -70,13 +72,14 @@ export default function AuthPage() {
             className="mt-5 mb-16 mx-auto"
           />
 
-          <label className="mb-2">کد اعتبارسنجی را وارد کنید</label>
+          <label className="mb-2">کد تایید برای شماره {phone} پیامک شد</label>
 
           <input
             type="text"
             className="bg-light-gray rounded-lg p-4 outline-none mb-7"
-            placeholder="شماره موبایل خود را وارد کنید"
-            {...register("phone", { required: true })}
+            placeholder="کد تایید را وارد کنید"
+            maxLength={5}
+            {...register("code", { required: true })}
           />
 
           <button className="bg-black text-white rounded-lg p-4 outline-none">
